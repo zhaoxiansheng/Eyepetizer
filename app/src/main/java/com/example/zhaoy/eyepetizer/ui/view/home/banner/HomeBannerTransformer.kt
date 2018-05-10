e07c7cf91505310289b98ffa0bb6766842f65bb8
zhaoy <bqzhaoyu@gmail.com>
2018-05-10 16:47:24 +0800

    首页完成

---
+++ app/src/main/java/com/example/zhaoy/eyepetizer/ui/view/home/banner/HomeBannerTransformer.kt
@@ -0,0 +1,17 @@
+package com.example.zhaoy.eyepetizer.ui.view.home.banner
+
+import android.support.v4.view.ViewPager
+import android.view.View
+
+class HomeBannerTransformer : ViewPager.PageTransformer {
+    override fun transformPage(page: View, position: Float) {
+        val width: Int = page?.width!!
+        //以向左滑动为例
+//        if (position <= 0) {//中间的
+        page.scrollX = (position * width).toInt() / 4 * 3
+//        } else if (position <= 1) {//右侧的
+//            page?.scrollX = (position * width).toInt() / 4 * 3
+
+//        }
+    }
+}
